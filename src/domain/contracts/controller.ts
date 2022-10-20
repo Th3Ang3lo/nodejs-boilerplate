66e0d1049b9230a2660a701062ff7277c7ffd379
Angelo Soares <angeelosoares@gmail.com>
2022-10-20 19:43:53 -0300

    refactor: request handler type

--- src/domain/contracts/controller.ts
+++ src/domain/contracts/controller.ts
@@ -1,3 +1,5 @@
+import { RequestHandler as ExpressRequestHandler } from 'express'
+
 export interface HttpParams<T = Record<string, any>> { params: T }
 export interface HttpBody<T = Record<string, any>> { body: T }
 export interface HttpHeaders<T = Record<string, any>> { headers: T }
@@ -5,13 +7,9 @@ export interface HttpQuery<T = Record<string, any>> { query: T }
 export interface HttpFile<T = Express.Multer.File> { file?: T }
 export interface HttpFiles<T = Express.Multer.File[]> { files?: T }
 
+export type RequestHandler = ExpressRequestHandler
+
 export type Request = Partial<HttpParams & HttpBody & HttpHeaders & HttpQuery & HttpFile & HttpFiles>
-
-export interface HttpResponse {
-  statusCode: number
-  data: any
-}
-
 export type Response = Record<string, any>
 
 export interface Controller {
--- src/infra/factories/controllers.ts
+++ src/infra/factories/controllers.ts
@@ -1,11 +1,11 @@
 import { TestController } from '@/controllers/test-controller'
-import { Response } from '@/domain/contracts/controller'
+import { RequestHandler } from '@/domain/contracts/controller'
 
 import { controllerAdapter as adapter } from '@/infra/http/adapters/controller'
 
 import { testUseCase } from './usecases'
 
-export const testController: Response = () => {
+export const testController = (): RequestHandler => {
   return adapter(
     new TestController(
       testUseCase()
